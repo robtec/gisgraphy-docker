d2a17678016aea9f712cb06010ddcd47d902dab3
Rob <robtec@users.noreply.github.com>
2022-11-01 02:51:34 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 set -e
+
+psql -Upostgres -h $PG_HOST -d gisgraphy -f /usr/local/gisgraphy/sql/create_tables.sql
+psql -Upostgres -h $PG_HOST -d gisgraphy -f /usr/local/gisgraphy/sql/insert_users.sql
+psql -Upostgres -h $PG_HOST -d gisgraphy -f /usr/local/gisgraphy/sql/createGISTIndex.sql
